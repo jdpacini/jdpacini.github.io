e875dea56165ea5018aa343663e89f09db4ac36e
Jason Pacini <jasonpacini@jasons-mbp.lan>
2020-07-27 18:58:22 -0600

    Updates

--- js/menu.js
+++ js/menu.js
@@ -2,6 +2,18 @@ document.querySelector('.menu').addEventListener('click', () => {
   document.querySelector('header').classList.toggle('show');
   document.querySelector('.menu .close').classList.toggle('show');
   document.querySelector('.menu .bars').classList.toggle('show');
+
+  document.querySelector('body').addEventListener('click', nextClick);
+  
+  function nextClick(e) {
+      if (e.target.hash) {
+        document.querySelector('header').classList.remove('show');
+        document.querySelector('.menu .close').classList.remove('show');
+        document.querySelector('.menu .bars').classList.remove('show');
+        document.querySelector('body').removeEventListener(nextClick);
+      }
+  }
+
 });
 
 document.querySelector('body').addEventListener('click', (e) => {
